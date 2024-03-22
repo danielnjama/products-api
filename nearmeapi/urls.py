4d5478b551e2055685e0d86b5729e0cfd8b9be49
Daniel Njama Wangari <daniel.njama@prodapt.com>
2024-03-22 09:51:16 +0300

    static files

--- nearmeapi/urls.py
+++ nearmeapi/urls.py
@@ -1,5 +1,8 @@
+from django.urls import path,include,re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 from django.contrib import admin
-from django.urls import path
 from rest_framework.authtoken import views as auth_views
 from index.views import UserInfoListCreate, UserInfoDetail, ProductCategoryListCreate, ProductCategoryDetail, ProductListCreate, ProductDetail, \
 CustomRegisterView, CustomLogoutView
@@ -20,4 +23,9 @@ urlpatterns = [
     path('api/login/', auth_views.obtain_auth_token, name='login'),
     path('api/register/', CustomRegisterView.as_view(), name='register'),
     path('api/logout/', CustomLogoutView.as_view(), name='logout'),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns= urlpatterns+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
